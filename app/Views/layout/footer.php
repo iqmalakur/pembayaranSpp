447fa20c5dea726475b39b3b9019862c10da520a
iqmalakbarkurnia <iakbarkurnia@gmail.com>
2021-03-04 17:12:05 +0700

    Menambahkan Library

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,5 +1,11 @@
 <!-- Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+<!-- Sweet Alert 2 -->
+<script src="/assets/package/dist/sweetalert2.all.js"></script>
+
+<!-- Chart.js -->
+<script src="/assets/Chart.js-2.9.4/dist/Chart.js"></script>
 </body>
 
 </html>
--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
+    <!-- Sweet Alert 2 CSS -->
+    <link rel="stylesheet" href="/assets/package/dist/sweetalert2.min.css">
+
     <title>Hello, world!</title>
 </head>
 
